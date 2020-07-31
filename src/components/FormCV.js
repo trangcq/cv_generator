b9633d883bdbe9e48deccc252d6481dadd2d42e7
trangcq <trangcq@rikkeisoft.com>
2020-07-31 17:11:00 +0700

    add template 2

--- src/components/FormCV.js
+++ src/components/FormCV.js
@@ -44,7 +44,6 @@ class FormCV extends Component {
     }
 
     render() {
-        const { users } = this.props;
         return (
             <div className="panel panel-default">
                 <div className="panel-heading">
--- src/components/Template.js
+++ src/components/Template.js
@@ -1,68 +1,15 @@
 import React, { Component } from "react";
-import '../assets/Template.css';
+import Template1 from './Template1';
+import Template2 from './Template2';
 
 class Template extends Component {
-
     render() {
         const { users } = this.props;
-        console.log(users);
-        return (
-            <div id="cvo-document" className="cvo-document">
-                <div>
-                    <div className="cvo-page">
-                        <div className="cvo-subpage">
-                            <div id="cvo-body">
-                                <div id="cvo-main">
-                                    <div id="group-header"><div id="cvo-profile" className="cvo-block">
-                                        <div id="cvo-header" className="hide-avatar">
-                                            <h1><span id="cvo-profile-fullname">{users ? users.name : ''}</span></h1>
-                                            <p>
-                                                <span id="cvo-profile-address">{users ? users.address : ''}</span>
-                                            </p>
-                                            <p>
-                                                <i className="fa fa-envelope"></i>
-                                                <span id="cvo-profile-email">{users ? users.email : ''}</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                    <div id="group-content" className="cvo-block">
-                                        <div className="cvo-block-title">
-                                            <h3><span>Mục tiêu nghề nghiệp</span></h3>
-                                        </div>
-                                        <div className="block-content">
-                                            <div className="cvo-block-body">
-                                                <div id="cvo-objective-objective">{users ? users.career : ''}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div id="group-content" className="cvo-block">
-                                        <div className="cvo-block-title">
-                                            <h3><span>Học vấn</span></h3>
-                                        </div>
-                                        <div className="block-content">
-                                            <div className="cvo-block-body">
-                                                <div id="cvo-objective-objective">{users ? users.education : ''}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div id="group-content" className="cvo-block">
-                                        <div className="cvo-block-title">
-                                            <h3><span>Kinh nghiệm làm việc</span></h3>
-                                        </div>
-                                        <div className="block-content">
-                                            <div className="cvo-block-body">
-                                                <div id="cvo-objective-objective">{users ? users.experience : ''}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
+        const isTemplate = users ? (users.template == 1 ? true : false) : false;
+        if (isTemplate) {
+            return <Template2 users={users} />;
+        }
+        return <Template1 users={users}/>;
     }
 }
 
